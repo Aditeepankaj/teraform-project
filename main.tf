b6a21aac1dc24e0ab482b4e38e402e8573767384
aditeepankaj30@gmail.com <aditeepankaj30@gmail.com>
2025-07-23 10:00:04 +0000

    latest

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "s3bucket" {
-    bucket = "awsterrabucket30"
+resource "aws_s3_bucket" "s3bucket6" {
+    bucket = "awsterrabucket23530"
     acl    = "private"
  
     tags = {
-        Name        = "bucketterra"
+        Name        = "buckets3terra"
         Environment = "Dev"
     }
 }
